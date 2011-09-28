e5f999ff198a1a272adceec6f2471a9adec8fe77
Matthieu Moy <Matthieu.Moy@imag.fr>
2011-09-28 23:39:37 +0200

    Slides C++

---
+++ TPs/setup-ensisun.sh
@@ -0,0 +1,9 @@
+#! /bin/echo Non, il ne faut pas executer ce fichier, mais faire: source
+
+export SYSTEMCROOT=~moy/tlm/systemc-2.2.0/
+export TLMOSCI=~moy/tlm/TLM-2009-07-15/
+export TLM_ENSITLM=~moy/tlm/ensitlm
+export SC_SIGNAL_WRITE_CHECK=DISABLE
+
+# Chaine de cross-compilation MicroBlaze, pour le TP3
+source /home/perms/moy/petalinux-v0.40-final/settings-moy.sh
